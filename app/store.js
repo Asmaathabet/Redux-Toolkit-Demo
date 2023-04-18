db19404cb1b2fe75e17a2555137a00e35acac136
AsmaaThabet <athabet@lucidya.com>
2023-04-18 17:22:56 +0300

    add logger to redux

--- app/store.js
+++ app/store.js
@@ -1,16 +1,32 @@
 import toolkit from "@reduxjs/toolkit";
 import cakeSlice from "../features/cake/cakeSlice.js";
 import iceCreamSlice from "../features/icecream/iceCreamSlice.js";
+import logger from 'redux-logger';
 
 const { configureStore } = toolkit;
+const { createLogger } = logger;
 const { reducer: cakeReducers } = cakeSlice;
 const { reducer: iceCreamReducers } = iceCreamSlice;
+
+
+const middleware = [];
+middleware.push(createLogger());
+const enhancers = [...middleware];
+
 
 const store = configureStore({
     reducer: {
         cake: cakeReducers,
-        icecream: iceCreamReducers
+        icecream: iceCreamReducers,
     },
+    middleware: enhancers,
+    // middleware: (getDefaultMiddleware) => {
+    //     let middleware = getDefaultMiddleware({
+    //         serializableCheck: false,
+    //     });
+    //     middleware = middleware.concat(logger);
+    //     return middleware;
+    // },
 })
 
 // module.exports = store;
